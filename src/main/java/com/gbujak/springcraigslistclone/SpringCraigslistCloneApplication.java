aa5956c15a583bbdbe68245518e5832e1b352a8d
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-08-12 21:39:43 +0200

    Initial commit

---
+++ src/main/java/com/gbujak/springcraigslistclone/SpringCraigslistCloneApplication.java
@@ -0,0 +1,13 @@
+package com.gbujak.springcraigslistclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCraigslistCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCraigslistCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbujak/springcraigslistclone/SpringCraigslistCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbujak.springcraigslistclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCraigslistCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
