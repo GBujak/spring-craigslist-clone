d7ea942bb852dc7fe9030371141965411c97dc11
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-08-17 22:51:28 +0200

    Add initial security config

--- src/main/java/com/gbujak/springcraigslistclone/controller/CategoryController.java
+++ src/main/java/com/gbujak/springcraigslistclone/controller/CategoryController.java
@@ -1,15 +1,19 @@
 package com.gbujak.springcraigslistclone.controller;
 
+import javassist.NotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/category/")
+@RequestMapping("category")
 public class CategoryController {
     @GetMapping("{category}")
-    public String getListings(@PathVariable String category) {
+    public String getListings(@PathVariable String category,
+                              Model model) throws NotFoundException {
+        model.addAttribute("test", "category test");
         return "category";
     }
 }
