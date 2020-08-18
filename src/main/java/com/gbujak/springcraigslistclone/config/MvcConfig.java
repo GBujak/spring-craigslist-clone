b9fc5cd036224a65bc2012e95ab15e618ad32dda
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-08-18 19:06:40 +0200

    Change MvcConfig

--- src/main/java/com/gbujak/springcraigslistclone/config/MvcConfig.java
+++ src/main/java/com/gbujak/springcraigslistclone/config/MvcConfig.java
@@ -13,13 +13,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // TODO: make this work (maybe amazon s3?)
         registry
                 .addResourceHandler("/uploads/**")
-                .addResourceLocations("/opt/uploads");
+                .addResourceLocations("file:/opt/uploads");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(16);
+        return new BCryptPasswordEncoder();
     }
 }
