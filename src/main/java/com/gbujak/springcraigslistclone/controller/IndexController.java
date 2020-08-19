25f7e73398dcd316b938d1406ab74a8260efc1b7
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-08-19 21:36:28 +0200

    Add slugs to AdvertisementCategory instances

--- src/main/java/com/gbujak/springcraigslistclone/controller/IndexController.java
+++ src/main/java/com/gbujak/springcraigslistclone/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -14,10 +15,7 @@ import java.util.stream.Collectors;
 public class IndexController {
     @GetMapping
     public String index(Model model) {
-        var categories = Arrays.stream(AdvertisementCategory.values())
-                .map(AdvertisementCategory::getName)
-                .collect(Collectors.toList());
-        model.addAttribute("categories", categories);
+        model.addAttribute("categories", AdvertisementCategory.values());
         return "index";
     }
 }
--- src/main/java/com/gbujak/springcraigslistclone/model/AdvertisementCategory.java
+++ src/main/java/com/gbujak/springcraigslistclone/model/AdvertisementCategory.java
@@ -1,19 +1,25 @@
 package com.gbujak.springcraigslistclone.model;
 
 public enum AdvertisementCategory {
-    COMMUNITY("społeczność"),
-    FOR_SALE("na sprzedaż"),
-    JOBS("praca"),
-    HOUSING("mieszkanie"),
-    SERVICES("usługi");
+    COMMUNITY("społeczność", "spolecznosc"),
+    FOR_SALE("na sprzedaż", "na-sprzedaz"),
+    JOBS("praca", "praca"),
+    HOUSING("mieszkanie", "mieszkanie"),
+    SERVICES("usługi", "uslugi");
 
     private final String name;
+    private final String slug;
 
-    AdvertisementCategory(String name) {
+    AdvertisementCategory(String name, String slug) {
         this.name = name;
+        this.slug = slug;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getSlug() {
+        return slug;
     }
 }
