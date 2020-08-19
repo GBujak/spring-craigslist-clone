c6d6bceb1e708e9ecac6cd58f5b7028a9d8ca642
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-08-19 19:58:12 +0200

    Add category links to index page

--- src/main/java/com/gbujak/springcraigslistclone/controller/IndexController.java
+++ src/main/java/com/gbujak/springcraigslistclone/controller/IndexController.java
@@ -1,14 +1,23 @@
 package com.gbujak.springcraigslistclone.controller;
 
+import com.gbujak.springcraigslistclone.model.AdvertisementCategory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping({"", "/", "index"})
 public class IndexController {
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        var categories = Arrays.stream(AdvertisementCategory.values())
+                .map(AdvertisementCategory::getName)
+                .collect(Collectors.toList());
+        model.addAttribute("categories", categories);
         return "index";
     }
 }
