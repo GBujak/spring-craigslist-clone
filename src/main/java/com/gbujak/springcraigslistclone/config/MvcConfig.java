f5c852127bddcc4cf96b67c3e7268f1fc6cd391b
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-08-21 15:11:00 +0200

    Display images in advertisement.html template

--- src/main/java/com/gbujak/springcraigslistclone/config/MvcConfig.java
+++ src/main/java/com/gbujak/springcraigslistclone/config/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.gbujak.springcraigslistclone.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,16 +8,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${imageStorageDirectory}")
+    private String imageDirectory;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // TODO: make this work (maybe amazon s3?)
         registry
-                .addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/opt/uploads");
+                .addResourceHandler("/zdjecie/**")
+                .addResourceLocations("file:" + imageDirectory);
     }
 
     @Bean
