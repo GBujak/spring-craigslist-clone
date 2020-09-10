ef2da89eeb87653426a0fe43e6cca6dbc67adcea
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-09-10 15:48:50 +0200

    Fix logging out

--- src/main/java/com/gbujak/springcraigslistclone/config/SecurityConfig.java
+++ src/main/java/com/gbujak/springcraigslistclone/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .logout()
-                    .permitAll();
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login");
     }
-
-    
 }
